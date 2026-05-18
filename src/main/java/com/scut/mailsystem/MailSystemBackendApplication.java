15cc7ec9ad908f870aba3f62b221ba35d781082b
songsuijie <2089970058@qq.com>
2026-05-18 14:08:07 +0800

    构建了后端骨架

---
+++ src/main/java/com/scut/mailsystem/MailSystemBackendApplication.java
@@ -0,0 +1,12 @@
+package com.scut.mailsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailSystemBackendApplication.class, args);
+    }
+}
